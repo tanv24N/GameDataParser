5d48d7ae41270d9aaab0f952d88705899aa64109
Tanvi <143971754+tanv24N@users.noreply.github.com>
2025-07-24 21:50:22 -0400

    Add files via upload

---
+++ Logging/Logger.cs
@@ -0,0 +1,23 @@
+﻿internal class Logger
+{
+    private string _logFileName;
+
+    public Logger(string logFileName)
+    {
+        _logFileName = logFileName;
+    }
+
+    public void Log(Exception ex)
+    {
+        var entry =
+$@"[{DateTime.Now}]
+Exception Message : {ex.Message}
+Stack : {ex.StackTrace}
+
+
+";
+
+        File.AppendAllText(_logFileName, entry);
+    }
+
+}
