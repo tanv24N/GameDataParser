d1daacd07de317c55a866daeb7872beb61bee187
Tanvi <143971754+tanv24N@users.noreply.github.com>
2025-07-24 21:51:54 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+using static GameDataParserApp;
+
+var userInteractor = new ConsoleUserInteraction();
+var gameDataPaerserObject = new GameDataParserApp(userInteractor, new GamesPrinter(userInteractor),
+    new VideoGameDeserializer(userInteractor), new LoadFileReader());
+var logger = new Logger("log.txt");
+
+try
+{
+    gameDataPaerserObject.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("The app ran into an unexpected error and will be closed.");
+    logger.Log(ex);
+}
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+
