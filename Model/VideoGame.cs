aabfa3651a8abc35c5b9a8a87bc87e86bb1303e5
Tanvi <143971754+tanv24N@users.noreply.github.com>
2025-07-24 21:50:49 -0400

    Add files via upload

---
+++ Model/VideoGame.cs
@@ -0,0 +1,28 @@
+﻿public class VideoGame
+{
+    public string Title { get; init; }
+    public int ReleaseYear { get; init; }
+    public decimal Rating { get; init; }
+
+    public override string ToString()
+    {
+        return $"{Title}, {ReleaseYear}, {Rating}";
+    }
+}
+
+
+//Alternate try catch block for Run() method code instead of If statements, in case we want to specify exceptions
+//try
+//catch (ArgumentNullException ex)
+//{
+//    Console.WriteLine("The file name cannot be null.");
+//}
+//catch (ArgumentException ex)
+//{
+//    Console.WriteLine("The file name cannot be empty.");
+//}
+//catch (FileNotFoundException ex)
+//{
+//    Console.WriteLine("The file name cannot be found.");
+//}
+
