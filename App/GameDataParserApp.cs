35eca8513a9c44736c2502cae1a47eab7e634098
Tanvi <143971754+tanv24N@users.noreply.github.com>
2025-07-24 21:49:09 -0400

    Add files via upload

---
+++ App/GameDataParserApp.cs
@@ -0,0 +1,43 @@
+﻿public class GameDataParserApp
+{
+    private readonly IUserInterface _userInteractor;
+    private readonly IGamesPrinter _gamesPrinter;
+    private readonly IVideoGameDeserializer _videoGameDeserializer;
+    private readonly IFileReader _fileReader;
+
+    public GameDataParserApp(IUserInterface userInteractor, IGamesPrinter gamesPrinter,
+        IVideoGameDeserializer videoGameDeserializer, IFileReader fileReader)
+    {
+        _userInteractor = userInteractor;
+        _gamesPrinter = gamesPrinter;
+        _videoGameDeserializer = videoGameDeserializer;
+        _fileReader = fileReader;
+    }
+
+
+    public void Run()
+    {
+        string fileName = _userInteractor.ReadValidFilePathFromUser();
+
+        var fileContents = _fileReader.Read(fileName);
+        List<VideoGame> videoGames = _videoGameDeserializer.DeserializeFromString(fileName, fileContents);
+        _gamesPrinter.Print(videoGames);
+    }
+}
+
+
+//Alternate try catch block for Run() method code instead of If statements, in case we want to specify exceptions
+//try
+//catch (ArgumentNullException ex)
+//{
+//    Console.WriteLine("The file name cannot be null.");
+//}
+//catch (ArgumentException ex)
+//{
+//    Console.WriteLine("The file name cannot be empty.");
+//}
+//catch (FileNotFoundException ex)
+//{
+//    Console.WriteLine("The file name cannot be found.");
+//}
+
